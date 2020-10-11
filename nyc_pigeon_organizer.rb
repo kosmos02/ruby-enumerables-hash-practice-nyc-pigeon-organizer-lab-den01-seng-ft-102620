2dd3d8c5cfe08396eb6ecc4fde9dfea45ccfe7fd
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-11 17:39:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,8 +19,10 @@ def nyc_pigeon_organizer(data)
       end 
       
       if !final_array[name][key]
-        final_array[name][key]
-      binding.pry
+        
+        final_array[name][key] = []
+      
+      
      
      end  
     end 
